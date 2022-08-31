4a2779f0ecc02aeb4b84d8911a4b28b836c7d076
Gaurav-jee <sgrssgrs@gmail.com>
2022-08-31 17:31:19 +0530

    Added my project

---
+++ forLoop.py
@@ -0,0 +1,26 @@
+
+# arr = {1, 2, 3, 4, ,5 ... };
+
+
+# N = 10;
+
+# for(int i=0; i<N; i++){
+#     print('Hello')
+#     print(arr[i]);
+# }
+
+
+ls = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+for iterator in ls:
+    print(iterator)
+
+rl = range(100)
+
+# print(rl)
+
+
+for i in range(100):
+    print(i)
+
+
